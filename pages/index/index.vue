c868d06b94ad95cfcb2c1f9eb0e469513c6323f1
cyy <2427811980@qq.com>
2025-07-16 13:52:52 +0800

    init：初始化项目

---
+++ pages/index/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// 响应式数据
+const title = ref('商品库存')
+
+// 生命周期钩子
+onMounted(() => {
+  // 原 onLoad 中的逻辑可放在这里
+})
+
+// 方法定义
+// const someMethod = () => { ... }
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
